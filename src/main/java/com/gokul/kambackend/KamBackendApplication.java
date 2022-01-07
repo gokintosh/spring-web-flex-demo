a2cfbf303813eaf31c5fba732beef0579f05d877
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2022-01-07 19:13:16 +0100

    init git

---
+++ src/main/java/com/gokul/kambackend/KamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.kambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamBackendApplication.class, args);
+    }
+
+}
